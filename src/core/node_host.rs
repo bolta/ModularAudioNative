f530610e19434991ee5863f9d41589d5a4649a64
bolta <freshness.and.speed.2007@gmail.com>
2021-01-02 03:16:06 +0900

    portaudio を使って音を鳴らすようにした

--- src/core/node_host.rs
+++ src/core/node_host.rs
@@ -1,20 +1,22 @@
 use super::node::*;
 use super::node_handle::*;
-use std::rc::Rc;
-use std::rc::Weak;
 use std::cell::RefCell;
+use std::rc::Rc;
+
+// exper
+extern crate portaudio;
+use portaudio as pa;
 
 pub struct NodeHost {
 	nodes: Vec<Node>,
 }
 
 impl NodeHost {
-		pub fn new() -> Self {
-		Self { nodes: vec![], }
+	pub fn new() -> Self {
+		Self { nodes: vec![] }
 	}
 
 	pub fn add_node(&mut self, node: Node) -> NodeHandle {
-
 		self.nodes.push(node);
 
 		NodeHandle {
@@ -23,8 +25,12 @@ impl NodeHost {
 		}
 	}
 
-	pub fn node(&self, id: usize) -> &Node { & self.nodes[id] }
-	pub fn node_mut(&mut self, id: usize) -> &mut Node { &mut self.nodes[id] }
+	pub fn node(&self, id: usize) -> &Node {
+		&self.nodes[id]
+	}
+	pub fn node_mut(&mut self, id: usize) -> &mut Node {
+		&mut self.nodes[id]
+	}
 
 	pub fn update(&mut self) {
 		for n in &mut self.nodes {
@@ -32,24 +38,61 @@ impl NodeHost {
 		}
 	}
 
-	pub fn play(&mut self) {
+	// TODO オーディオと密結合させない
+	pub fn play(this: Rc<RefCell<Self>>) {
+		let node_exists = this.borrow().nodes.len() > 0;
+		if ! node_exists {
+			panic!("no nodes to play");
+		}
+
+		// コールバックの中から Self のメソッドを呼ぶ関係で、
+		// 
+		let pa = pa::PortAudio::new().expect("failed to initialise PortAudio");
+
 		let play_sample = |value: f32| {
 			println!("{}", value);
 		};
 
-		// let last = { (self /* as &mut Self */).nodes.last_mut() };
-		let node_exists = self.nodes.len() > 0;
-		if ! node_exists {
-			panic!("no nodes to play");
-		} else {
-			loop {
+		const SMP_RATE: i32 = 44100;
+		const QUANT_RATE: i32 = 16;
+		const CHANNELS: i32 = 1;
+		const FRAMES_PER_BUFFER: u32 = 64;
+
+		let mut settings =
+				pa.default_output_stream_settings(CHANNELS, SMP_RATE as f64, FRAMES_PER_BUFFER)
+				.expect("failed to initialise settings");
+		// we won't output out of range samples so don't bother clipping them.
+		settings.flags = pa::stream_flags::CLIP_OFF;
+
+		// This routine will be called by the PortAudio engine when audio is needed. It may called at
+		// interrupt level on some machines so don't do anything that could mess up the system like
+		// dynamic resource allocation or IO.
+		let callback = move |pa::OutputStreamCallbackArgs { buffer, frames, .. }| {
+			let mut idx = 0;
+			for _ in 0..frames {
 				//_ ここ（self.update() より前）に書くとエラーになる。
 				// 厳しすぎないか？　毎回正直に last().unwrap() すると
 				// パフォーマンスに悪そうなんだけど…
 				// let master = self.nodes.last().unwrap();
-				self.update();
-				(play_sample)(self.nodes.last().unwrap().current());
+				this.borrow_mut().update();
+				let value = this.borrow().nodes.last().unwrap().current();
+				buffer[idx] = value;
+				idx += 1;
 			}
-		}
+			pa::Continue
+		};
+
+		let mut stream = pa.open_non_blocking_stream(settings, callback)
+				.expect("failed to open stream");
+
+		// TODO エラー処理
+		stream.start();
+
+		const NUM_SECONDS: i32 = 2;
+		println!("Play for {} seconds.", NUM_SECONDS);
+		pa.sleep(NUM_SECONDS * 1_000);
+
+		stream.stop();
+		stream.close();
 	}
 }
--- src/main.rs
+++ src/main.rs
@@ -7,18 +7,34 @@ use crate::core::node_host::*;
 
 fn main() {
 	let mut host = NodeHost::new();
-	let mut var = 0f32;
-	let gen = host.add_node(Node::from_closure(Box::new(move || {
-		let result = var;
-		var += 0.01f32;
-		Some(result)
+	// let mut var = 0f32;
+	// let gen = host.add_node(Node::from_closure(Box::new(move || {
+	// 	let result = var;
+	// 	var += 0.01f32;
+	// 	Some(result)
+	// })));
+
+	// let neg = gen.negate();
+	// let konst = NodeHandle::constant(&mut host, 100f32);
+
+	// // 100 から減っていく
+	// let sum = neg.add(&konst);
+
+	const SMP_RATE: i32 = 44100;
+	const QUANT_RATE: i32 = 16;
+	const CHANNELS: i32 = 1;
+	let freq = 440_f32;
+
+	let mut phase = 0_f32;
+
+	let osc = host.add_node(Node::from_closure(Box::new(move || {
+		let value = phase.sin();
+		phase += (2_f32 * std::f32::consts::PI) * freq / (SMP_RATE as f32);
+		phase %= 2_f32 * std::f32::consts::PI;
+		Some(value)
 	})));
 
-	let neg = gen.negate();
-	let konst = NodeHandle::constant(&mut host, 100f32);
-
-	// 100 から減っていく
-	let sum = neg.add(&konst);
-
-	host.play();
+	let host_shared = std::rc::Rc::new(std::cell::RefCell::new(host));
+	NodeHost::play(host_shared);
 }
+
