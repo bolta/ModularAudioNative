f37942d515358b1430019e997a412e67e0accf78
bolta <freshness.and.speed.2007@gmail.com>
2021-01-05 00:58:15 +0900

    デバッグ出力を削除

--- src/node/util.rs
+++ src/node/util.rs
@@ -3,10 +3,5 @@ use crate::core::node::*;
 /// 安全なはずだが寿命チェックに引っかかってしまう場合、
 /// 一旦ポインタにすることでチェックを逃れる（こんな方法しかないのか？？）
 pub fn discard_lifetime<'a>(node: &'a Node) -> &'static Node {
-println!("{:p} -> {:p} -> {:p}",
-		node,
-		node as *const Node,
-		// * (node as *const Node),
-		unsafe { &* (node as *const Node) });	
 	unsafe { &* (node as *const Node) }
 }
