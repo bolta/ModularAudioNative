4e0770e41fbbb3a386499156e6d4a2a766dc0d89
bolta <freshness.and.speed.2007@gmail.com>
2021-05-02 23:23:43 +0900

    発音コマンドを途中まで実装（音長とスラーはまだ）

--- src/mml/default/default_mml_parser.rs
+++ src/mml/default/default_mml_parser.rs
@@ -18,6 +18,8 @@ use combine::{
 	error::ParseError,
 	many,
 	many1,
+	one_of,
+	optional,
 	Parser,
 	parser::{
 		char::{
@@ -76,7 +78,7 @@ pub enum Command {
 	Volume(f32),
 	Velocity(f32),
 	Detune(f32),
-	Tone { tone_name: ToneName, length: Length, slur: bool },
+	Tone { tone_name: ToneName, length: Option<Length>, slur: bool },
 	Rest(Length),
 	Parameter { name: String, value: f32 },
 	Loop { times: Option<i32>, Content: Vec<Command> },
@@ -122,6 +124,7 @@ where
 
 	// 後続の空白を食うパーザたち
 	let integer_ss = || integer().skip(skip_spaces());
+	let one_of_ss = |toks| one_of(toks).skip(skip_spaces());
 	let real_ss = || real().skip(skip_spaces());
 	let string_ss = |tok| string(tok).skip(skip_spaces());
 	let token_ss = |tok| token(tok).skip(skip_spaces());
@@ -143,7 +146,34 @@ where
 	let volume_command = real_command("V", Command::Volume);
 	let velocity_command = real_command("v", Command::Velocity);
 	let detune_command = real_command("@d", Command::Detune);
-	// let tone_command = 
+
+	// 言われるままに型注釈をつけた
+	// https://docs.rs/combine/4.5.2/combine/fn.many1.html
+	let accidentals = many1::<Vec<_>, _, _>(token_ss('+')).map(|sharps| sharps.len() as i32)
+			.or(many1::<Vec<_>, _, _>(token_ss('-')).map(|flats| - (flats.len() as i32)));
+	let tone_command = one_of_ss("cdefgab".chars())
+			.and(optional(accidentals))
+			.map(|(b, a)| {
+		let base_name = match b {
+			'c' => ToneBaseName::C,
+			'd' => ToneBaseName::D,
+			'e' => ToneBaseName::E,
+			'f' => ToneBaseName::F,
+			'g' => ToneBaseName::G,
+			'a' => ToneBaseName::A,
+			'b' => ToneBaseName::B,
+			_ => unreachable!(),
+		};
+
+		Command::Tone {
+			tone_name: ToneName {
+				base_name,
+				accidental: a.unwrap_or(0),
+			},
+			length: None,//Length { elements: vec![LengthElement] }
+			slur: false,
+		}
+	});
 	// let rest_command = 
 	// let parameter_command = 
 	// let loop_command = 
@@ -159,7 +189,8 @@ where
 			.or(gate_rate_command)
 			.or(volume_command)
 			.or(velocity_command)
-			.or(detune_command);
+			.or(detune_command)
+			.or(tone_command);
 
 	// 最先頭の空白だけここで食う
 	spaces()
@@ -222,4 +253,26 @@ fn test_compilation_unit_spaces() {
 		};
 		assert_eq!(compilation_unit().parse("< >").unwrap().0, expected);
 	}
+}
+
+#[test]
+fn test_compilation_unit_tones() {
+	let expected = |base_name, accidental, length, slur| CompilationUnit {
+		commands: vec![
+			Command::Tone {
+				tone_name: ToneName {
+					base_name,
+					accidental,
+				},
+				length,
+				slur,
+			},
+		]
+	};
+
+	assert_eq!(compilation_unit().parse("c ").unwrap().0, expected(ToneBaseName::C, 0, None, false));
+	assert_eq!(compilation_unit().parse("d+ ").unwrap().0, expected(ToneBaseName::D, 1, None, false));
+	assert_eq!(compilation_unit().parse("e ++ + ").unwrap().0, expected(ToneBaseName::E, 3, None, false));
+	assert_eq!(compilation_unit().parse("f -").unwrap().0, expected(ToneBaseName::F, -1, None, false));
+	assert_eq!(compilation_unit().parse("g -- - ").unwrap().0, expected(ToneBaseName::G, -3, None, false));
 }
