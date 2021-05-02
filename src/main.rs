6de2f62b9781d49b6aaeda5fa38cdc011164562d
bolta <freshness.and.speed.2007@gmail.com>
2021-05-02 19:03:29 +0900

    MML パーザを追加（実装途中・試行錯誤中）

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod mml;
 mod node;
 mod seq;
 
+<<<<<<< HEAD
 use crate::core::{
 	context::*,
 	machine::*,
@@ -57,6 +58,42 @@ fn main() {
 	// sched.add_event(44100, Box::new(SetEvent::new(EventTarget::Tag(String::from("freq")), 880f32)));
 	// sched.add_event(2 * 44100, Box::new(TerminateEvent { }));
 	// nodes.add(sched);
+=======
+use crate::core::node::*;
+use crate::core::node_handle::*;
+use crate::core::node_host::*;
+use crate::node::arith::*;
+use crate::node::osc::*;
+use crate::node::prim::*;
+use combine::Parser;
+
+fn main() {
+	// let mut host = NodeHost::new(44100, 16, 1);
+	// let freq = constant(&mut host, 440_f32);
+	// // let osc =
+	// // 		sin_osc(
+	// // 				add(
+	// // 						freq,
+	// // 						mul(
+	// // 								sin_osc(constant(&mut host, 1320_f32)),
+	// // 								constant(&mut host, 660_f32))));
+
+	// let osc =
+	// 		pulse_osc(constant(&mut host, 440_f32), constant(&mut host, 0.25_f32));
+
+
+
+	// 								let host_shared = std::rc::Rc::new(std::cell::RefCell::new(host));
+	// NodeHost::play(host_shared);
+
+//	println!("{}", crate::mml::default::default_mml_parser::parse_integer("-0123000"));
+	// crate::mml::default::default_mml_parser::boke();
+	let result = crate::mml::default::default_mml_parser::compilation_unit().parse("o4l8v15");
+	println!("{}", "hoge");
+	// println!("{}", result);
+	println!("{:?}", result);
+}
+>>>>>>> 10c7bc4... MML パーザを追加（実装途中・試行錯誤中）
 
 	let var = nodes.add_with_tag(String::from("freq"), Box::new(Var::new(0f32)));
 	let sin = nodes.add(Box::new(SineOsc::new(var)));
