24b1b80ab11c4b0f06877cf06c7fa24812fda294
bolta <freshness.and.speed.2007@gmail.com>
2021-07-04 03:39:49 +0900

    未使用の変数に対応

--- src/core/node.rs
+++ src/core/node.rs
@@ -14,7 +14,7 @@ pub trait Node {
 	fn initialize(&mut self) { }
 	fn upstreams(&self) -> Vec<NodeIndex>; // { vec![] }
 	fn execute(&self, inputs: &Vec<Sample>) -> Sample; // ここで状態を変えないといけない場合があるかも？
-	fn update(&mut self, inputs: &Vec<Sample>) { }
+	fn update(&mut self, _inputs: &Vec<Sample>) { }
 	fn finalize(&mut self) { }
 }
 
