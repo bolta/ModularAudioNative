9f41938cbbc68acea71e11c750ddf1e3b1ae33c6
bolta <freshness.and.speed.2007@gmail.com>
2021-07-22 15:36:36 +0900

    Value と Wait に対応

--- src/seq/instruction.rs
+++ src/seq/instruction.rs
@@ -7,5 +7,5 @@ pub enum Instruction {
 	// TODO tag は intern した文字列にする
 	Note { tag: String, note_on: bool },
 	Value { tag: String, value: Sample },
-	Wait { ticks: i32 },
+	Wait(i32),
 }
--- src/seq/sequencer.rs
+++ src/seq/sequencer.rs
@@ -4,6 +4,9 @@ use crate::core::{
 	node::*,
 	context::Context as CoreContext,
 };
+use crate::node::{
+	var::*,
+};
 use super::{
 	common::*,
 	context::*,
@@ -12,44 +15,65 @@ use super::{
 	sequence::*,
 };
 
-pub struct Sequencer/* <'a> */ {
-	// TODO 今後 sequence と context はともに任意個になる
-	sequence: Sequence,
-	context: Context/* <'a> */,
+pub struct Sequencer {
+	sequences: Vec<Sequence>,
+	// TODO 今後 context は任意個になる予定
+	context: Context,
 }
-impl /* <'a> */ Sequencer/* <'a> */ {
+impl Sequencer {
+	// TODO 今後 sequence は任意個になる予定
 	pub fn new(sequence: Sequence) -> Self {
 		Self {
-			sequence,
+			sequences: vec![sequence],
 			context: Context {
 				// ↓上の sequence と同時に初期化する方法がわからないのでとりあえず index で持つ
 				// sequence: self.sequence,
-				sequence: SequenceIndex(0),
-				instruction: InstructionIndex(0),
+				seq_idx: SequenceIndex(0),
+				instrc_idx: InstructionIndex(0),
+				wait: 0,
 			}
 		}
 	}
 }
 
-impl /* <'a> */ Node for Sequencer/* <'a> */ {
-	// fn upstreams(&self) -> 
+impl Node for Sequencer {
 	fn process_event(&mut self, event: &dyn Event, context: &CoreContext, env: &mut Environment) {
 		if event.event_type() != EVENT_TYPE_TICK { return; }
 
-		println!("tick at sample {}", context.elapsed_samples());
+//		println!("tick at sample {}", context.elapsed_samples());
+		self.context.tick(&self.sequences[self.context.seq_idx.0], context, env);
 	}
 }
 
-struct Context/* <'a> */ {
+struct Context {
 //	sequence: &'a Sequence,
-	sequence: SequenceIndex,
-	instruction: InstructionIndex,
+	seq_idx: SequenceIndex,
+	instrc_idx: InstructionIndex,
+	wait: i32,
 }
-// impl <'a> Context<'a> {
-// 	pub fn new(sequence: &'a Sequence) -> Self {
-// 		Self {
-// 			sequence,
-// 			index: InstructionIndex(0),
-// 		}
-// 	}
-// }
+impl Context {
+	fn tick(&mut self, sequence: &Sequence, context: &CoreContext, env: &mut Environment) {
+		if self.wait > 0 {
+			self.wait -= 1;
+			if self.wait > 0 { return; }
+		}
+
+		// ウェイトを挟まずに並んでいるインストラクションは全て実行する
+		while self.wait == 0 && self.instrc_idx.0 < sequence.len() {
+			match &sequence[self.instrc_idx.0] {
+				Instruction::Note { tag, note_on } => {
+					unimplemented!();
+				}
+				Instruction::Value { tag, value } => {
+					// TODO キューが一杯だったときの処理
+					env.events_mut().push(Box::new(SetEvent::new(EventTarget::Tag(tag.clone()), *value)));
+				}
+				Instruction::Wait(wait) => {
+					self.wait = *wait;
+				}
+
+			}
+			self.instrc_idx.0 += 1;
+		}
+	}
+}
