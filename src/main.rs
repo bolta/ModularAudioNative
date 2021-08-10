4b2a5eeceaac1247c6a87f1bfbc768f5398ea07d
bolta <freshness.and.speed.2007@gmail.com>
2021-08-10 19:47:49 +0900

    ステレオ用のサンプルコード

--- src/main.rs
+++ src/main.rs
@@ -29,7 +29,7 @@ use crate::seq::{
 };
 
 use crate::mml::default::{
-	// default_mml_parser::*,
+	default_mml_parser::*,
 	sequence_generator::*,
 };
 
@@ -37,35 +37,40 @@ use crate::moddl::{
 	error::*,
 	player,
 };
-// use combine::Parser;
+use combine::Parser;
 
 extern crate nom;
 
 
-// fn main() {
-// 	let mut machine = Machine::new();
-// 	let mut context = Context::new(44100, 1);
-// 	let mut nodes = NodeHost::new();
+fn main() {
+	let mut machine = Machine::new();
+	let mut context = Context::new(44100/* , 2 */);
+	let mut nodes = NodeHost::new();
 
-// 	let var = nodes.add_with_tag(String::from("freq"), Box::new(Var::new(0f32)));
-// 	let sin = nodes.add(Box::new(SineOsc::new(var)));
-// 	let env = nodes.add_with_tag(String::from("note"), Box::new(ExpEnv::new(0.125f32)));
-// 	let vol = nodes.add(Box::new(Constant::new(0.5f32)));
-// 	let master = nodes.add(Box::new(Mul::new(vec![sin, env, vol])));
-// 	nodes.add(Box::new(PortAudioOut::new(master, &context)));
+	let var = nodes.add_with_tag(String::from("freq"), Box::new(Var::new(0f32)));
+	let osc = nodes.add(Box::new(StereoTestOsc::new(var)));
+	// let env = nodes.add_with_tag(String::from("note"), Box::new(ExpEnv::new(0.125f32)));
+	// let vol = nodes.add(Box::new(Constant::new(0.5f32)));
+	// let master = nodes.add(Box::new(Mul::new(vec![osc, env, vol])));
+	let master = osc;
+	nodes.add(Box::new(PortAudioOut::new(master, nodes[master].channels(), &context)));
 
-// 	let ast = compilation_unit().parse("o5l16 efgrgrcr>c8<arf8dre8r8d48e48d8.^-24cc2").unwrap().0;
-// 	let seqs = generate_sequences(&ast, 96);
+	let ast = compilation_unit().parse("o5l16 efgrgrcr>c8<arf8dre8r8d48e48d8.^-24cc2").unwrap().0;
+	let tag_set = TagSet {
+		freq: "freq".to_string(),
+		note: "note".to_string(),
+	};
+	let seqs = generate_sequences(&ast, 96, &tag_set);
 
-// 	let tick = nodes.add(Box::new(Tick::new(80f32, 112, "seq".to_string())));
-// 	let seqr = nodes.add_with_tag("seq".to_string(), Box::new(Sequencer::new(seqs)));
+	let tick = nodes.add(Box::new(Tick::new(80f32, 112, "seq".to_string())));
+	let seqr = nodes.add_with_tag("seq".to_string(), Box::new(Sequencer::new(seqs)));
 
-// 	let mut sched = Box::new(EventScheduler::new());
-// 	sched.add_event(5 * context.sample_rate(), Box::new(TerminateEvent { }));
-// 	nodes.add(sched);
+	let mut sched = Box::new(EventScheduler::new());
+	sched.add_event(5 * context.sample_rate(), Box::new(TerminateEvent { }));
+	nodes.add(sched);
 
-// 	machine.play(&mut context, &mut nodes);
-// }
+	machine.play(&mut context, &mut nodes);
+}
 
 
 // fn main() {
@@ -86,11 +91,11 @@ extern crate nom;
 // }
 
 // use crate::moddl::parser::*;
-fn main() {
-	if let Err(e) = play() {
-		format!("error: {:?}", e);
-	}
-}
+// fn main() {
+// 	if let Err(e) = play() {
+// 		format!("error: {:?}", e);
+// 	}
+// }
 
 fn play() -> ModdlResult<()> {
 	player::play(r"
