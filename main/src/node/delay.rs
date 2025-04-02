bb437875eb249e8717f1291690c2eba8d8f09418
bolta <freshness.and.speed.2007@gmail.com>
2025-04-03 00:44:19 +0900

    dryWet 関数を作ったので、delay から wet を削除

--- main/src/node/delay.rs
+++ main/src/node/delay.rs
@@ -16,7 +16,6 @@ pub struct Delay {
 	signal: MonoNodeIndex,
 	time: MonoNodeIndex, // ディレイタイムは秒単位
 	feedback: MonoNodeIndex,
-	wet: MonoNodeIndex,
 }
 impl Delay {
 	pub fn new(
@@ -25,31 +24,32 @@ impl Delay {
 		signal: MonoNodeIndex,
 		time: MonoNodeIndex,
 		feedback: MonoNodeIndex,
-		wet: MonoNodeIndex,
 	) -> Self {
 		Self {
 			buffer: DelayBuffer::new((max_time * sample_rate as f32).ceil() as usize),
 			signal,
 			time,
 			feedback,
-			wet,
 		}
 	}
 }
 #[node_impl]
 impl Node for Delay {
 	fn channels(&self) -> i32 { 1 }
-	fn upstreams(&self) -> Upstreams { vec![self.signal.channeled(), self.time.channeled(), self.feedback.channeled(), self.wet.channeled()] }
+	fn upstreams(&self) -> Upstreams { vec![self.signal.channeled(), self.time.channeled(), self.feedback.channeled()] }
 	fn activeness(&self) -> Activeness { Activeness::Active }
 	fn execute(&mut self, inputs: &Vec<Sample>, output: &mut [Sample], context: &Context, _env: &mut Environment) {
 		let signal = inputs[0];
 		let time = inputs[1];
 		let feedback = inputs[2];
-		let wet = inputs[3];
 
 		let time_sample = ((time * context.sample_rate_f32()).round() as i32).min(self.buffer.len() as i32);
-		output_mono(output, signal + self.buffer[- (time_sample - 1)] * wet);
-		self.buffer.push(signal + feedback * self.buffer[- (time_sample - 1)]);
+		let delay = self.buffer[- (time_sample - 1)];
+
+		// 入力を含まない、バッファの値だけを出力する
+		output_mono(output, delay);
+		// 入力はバッファを一周してから出力されるよう、（フィードバックとともに）バッファに入れる
+		self.buffer.push(signal + feedback * delay);
 	}
 }
 pub struct DelayFactory {
@@ -65,7 +65,6 @@ impl NodeFactory for DelayFactory {
 	fn node_arg_specs(&self) -> Vec<NodeArgSpec> { vec![
 		spec_with_default("time", 1, self.max_time),
 		spec_with_default("feedback", 1, 0f32),
-		spec_with_default("wet", 1, 1f32),
 	] }
 	fn input_channels(&self) -> i32 { 1 }
 	fn create_node(&self, node_args: &NodeArgs, piped_upstream: ChanneledNodeIndex) -> Box<dyn Node> {
@@ -75,7 +74,6 @@ impl NodeFactory for DelayFactory {
 			piped_upstream.as_mono(),
 			node_args.get("time").unwrap().as_mono(),
 			node_args.get("feedback").unwrap().as_mono(),
-			node_args.get("wet").unwrap().as_mono(),
 		))
 	}
 }
