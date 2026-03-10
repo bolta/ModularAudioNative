d7e95d578678972fbc79a2cbaf107909ba8c2b89
bolta <freshness.and.speed.2007@gmail.com>
2026-03-11 01:04:47 +0900

    Revert "デバッグ出力を追加（無意味な変更だが、案件で使ったバージョンとして、記録のためコミットしておく）"
    This reverts commit e36536025108b29af6230534eba9230ccb4dac04.

--- main/src/seq/sequencer.rs
+++ main/src/seq/sequencer.rs
@@ -106,7 +106,6 @@ impl Context {
 
 				// シーケンスの終わりに達した
 				if self.stack.is_bottom() {
-println!("👻 {}", self.name);
 					env.broadcast_event(context.elapsed_samples(), Box::new(JobEvent::ended(self.name.clone())));
 					break; // 曲が終わった。次回の tick からは何もしない
 				} else {
